3786969cc175f816c53b2fc373f9757bede3b14c
koritafei <koritafei@gmail.com>
2023-07-25 01:13:45 +0000

    元数据结构算法

--- srcs/metaalgorithm/getquesize.cc
+++ srcs/metaalgorithm/getquesize.cc
@@ -53,6 +53,90 @@ struct At_<TCont<TCurrtype, TTypes...>, 0> {
   using type = TCurrtype;
 };
 
+/// tranform
+template <typename TInCont,
+          template <typename>
+          typename F,
+          template <typename...>
+          typename TOutCont>
+struct Transform_;
+
+template <template <typename...> typename TInCont,
+          typename... TInputs,
+          template <typename>
+          typename F,
+          template <typename...>
+          typename TOutCont>
+struct Transform_<TInCont<TInputs...>, F, TOutCont> {
+  using type = TOutCont<typename F<TInputs>::type...>;
+};
+
+template <typename TInCont,
+          template <typename>
+          typename F,
+          template <typename...>
+          typename TOutCont>
+using Transform = typename Transform_<TInCont, F, TOutCont>::type;
+
+// template <typename TFisrstSet, typename TSecondSet>
+// struct IsEqual_;
+
+// template <template <typename...> class Cont1,
+//           template <typename...>
+//           class Cont2,
+//           typename... Params1,
+//           typename... Params2>
+// struct IsEqual_<Cont1<Params1...>, Cont2<Params2...>> {
+//   constexpr static bool value1 = (HasKey<Cont1<Params1...>, Params2> && ...);
+//   constexpr static bool value2 = (HasKey<Cont2<Params2...>, Params1> && ...);
+//   constexpr static bool value  = value1 && value2;
+// };
+
+// template <typename TFirstSet, typename TSecondSet>
+// constexpr bool IsEqual = IsEqual_<TFirstSet, TSecondSet>::value;
+
+template <typename TState,
+          template <typename, typename>
+          typename F,
+          typename... Remain>
+struct imp_ {
+  using type = TState;
+};
+
+template <typename TState,
+          template <typename, typename>
+          typename F,
+          typename T0,
+          typename... Remain>
+struct imp_<TState, F, T0, Remain...> {
+  using type = typename imp_<F<TState, T0>, F, Remain...>::type;
+};
+
+template <typename TInitState,
+          typename TInputCont,
+          template <typename, typename>
+          typename F>
+struct Fold_;
+
+template <typename TInitState,
+          template <typename...>
+          class TCont,
+          typename... TParams,
+          template <typename, typename>
+          typename F>
+struct Fold_<TInitState, TCont<TParams...>, F> {
+  template <typename S, typename I>
+  using FF = typename F<S, I>::type;
+
+  using type = typename imp_<TInitState, FF, TParams...>::type;
+};
+
+template <typename TInitState,
+          typename TInputCont,
+          template <typename, typename>
+          typename F>
+using Fold = typename Fold_<TInitState, TInputCont, F>::type;
+
 int main() {
   using Cont            = std::tuple<char, double, int>;
   constexpr size_t Res1 = Size<Cont>;
